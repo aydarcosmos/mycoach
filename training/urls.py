66dc7e7d5226592336e89effee7a1e0122fa22bb
Aidar <gumerov1993@mail.ru>
2019-10-28 22:34:06 +0500

    Добавлена страница всех статей и картинка на главный экран(временно)

--- training/urls.py
+++ training/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import index
+from .views import index, posts_page
 
 urlpatterns = [
-    path('', index),
+    path('', index, name='index'),
+    path('posts/', posts_page, name = 'posts_page'), 
 ]
--- training/views.py
+++ training/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
-from .models import Sport
+from .models import Sport, Post
 
 # Create your views here.
 def index(request):
+    return render(request, 'training/index.html')
+
+def posts_page(request):
     sports = Sport.objects.all()
-    context = {'sports': sports}
-    return render(request, 'layout/basic.html', context)
+    posts = Post.objects.all()
+    context = {'sports': sports, 'posts': posts}
+    return render(request, 'training/all_posts.html', context)
