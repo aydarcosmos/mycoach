5fb9320d06c2df1285d7d2ff4099cd8751d52c06
Aidar <gumerov1993@mail.ru>
2019-10-28 16:36:29 +0500

    create new project with PostgreSQL in AWS cloud

---
+++ training/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+# Create your models here.
+class Post(models.Model):
+    name = models.CharField(max_length=50, verbose_name = 'Название статьи')
+    content = models.TextField(verbose_name = 'Статья')
+    published = models.DateTimeField(auto_now_add = True, db_index = True, verbose_name = 'Опубликовано')
+    sport = models.ForeignKey('Sport', on_delete = models.PROTECT, verbose_name = 'Вид спорта')
+
+    class Meta:
+        verbose_name = 'Пост'
+        verbose_name_plural = 'Посты'
+        ordering = ['-published']
+
+class Sport(models.Model):
+    name = models.CharField(max_length = 30, verbose_name = 'Вид спорта')
+
+    class Meta:
+        verbose_name = 'Вид спорта'
+        verbose_name_plural = 'Виды спорта'
+        ordering = ['name']
